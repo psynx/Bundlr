1d1d4771b2bc0bd49df4565c63d8a15ee9bbb956
psynx <89815919+psynx@users.noreply.github.com>
2021-09-30 14:34:45 -0400

    Update bundlr.js

--- bundlr.js
+++ bundlr.js
@@ -1,1 +1,17 @@
+import os
+import requests
 
+files = os.listdir()
+
+q = [i for i in files if i.endswith('.js')]
+for i in q:
+    open('bundle.min.js',
+         'a').write("\n /* COMPILED FROM " + i + " */" + open(i).read())
+
+import requests
+
+url = 'https://www.toptal.com/developers/javascript-minifier/raw'
+data = {'input': open('bundle.min.js', 'rb').read()}
+response = requests.post(url, data=data)
+
+open("bundle.min.js", 'w').write(response.text)
